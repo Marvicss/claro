875c95ab897a3f65ae5e46de3eab75609e3e0a1d
lucasbomfim15 <bmfmlucas@gmail.com>
2025-05-05 12:26:00 -0300

    feat: create product model

---
+++ src/main/java/com/claro/claro/modules/products/model/Product.java
@@ -0,0 +1,35 @@
+package com.claro.claro.modules.products.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private double price;
+}
