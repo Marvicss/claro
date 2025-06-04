59ee4956466cb7ff9eb1d7bb2625a3b6dfcb8aa1
Marvicss <marconivictor15@gmail.com>
2025-06-04 13:29:45 -0300

    add vercel shareble linnk to cors config

--- src/main/java/com/claro/claro/config/WebCOnfig.java
+++ src/main/java/com/claro/claro/config/WebCOnfig.java
@@ -15,7 +15,8 @@ public class WebCOnfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todas as rotas
                         .allowedOrigins("http://localhost:3000",
-                                "https://front-squad36-claro-cqh823zfs-vanessas-projects-3185b83b.vercel.app")
+                                "https://front-squad36-claro-cqh823zfs-vanessas-projects-3185b83b.vercel.app",
+                                "https://front-squad36-claro.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Se estiver usando cookies/autenticação
